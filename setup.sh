dcdc4c43a85e361b2d91816d920a4ee73b61f95c
greekbust <104328056+greek0x0@users.noreply.github.com>
2022-05-19 13:56:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 echo 'configuring vim..'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cp .vimrc $HOME
-cp -r .vim/ $HOME
+cp $HOME/Repo/vimrc/.vimrc $HOME
+cp -r $HOME/Repo/vimrc/.vim/ $HOME
 vim +PlugInstall +qal
 
